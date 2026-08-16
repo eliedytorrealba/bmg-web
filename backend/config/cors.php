a9e3c8c835d43e7d0a09a845d301728b57a746e4
Eliedy Torrealba <eliedytorrealba@gmail.com>
2026-08-16 18:36:05 -0300

    fix: allow production frontend in CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,9 +8,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_filter([
         'http://localhost:5173',
-    ],
+        env('FRONTEND_URL'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
